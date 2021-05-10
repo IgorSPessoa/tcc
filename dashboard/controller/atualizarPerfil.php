0ffd5589345c5099d1679cb102700257abdf8290
HenriqueBenemon <67244393+HenriqueBenemon@users.noreply.github.com>
2021-05-10 17:43:39 -0300

    perfil error
    file's change: dashboard\controller\atualizarPerfil, dashboard\perfil

--- dashboard/controller/atualizarPerfil.php
+++ dashboard/controller/atualizarPerfil.php
@@ -21,10 +21,78 @@ include '../../connect.php';
 //pegando o id da ong 
 $id = $_SESSION['id'];
 
+//pegando os dados do forms
+$proposito = "$_POST[reason]";
+$horarioFunc = "$_POST[opening_hours]";
+$telefone = "$_POST[phone]";
+$descricao = "$_POST[description]";
+$CEP = "$_POST[cep]";
+$rua = "$_POST[address]";
+$numero = "$_POST[number]";
+$bairro = "$_POST[district]";
+$estado = "$_POST[state]";
+
+//Colocando o estado(por escrito) no $estado, pois no VIACEP não está disponibilizado esta função (apenas em o UF)
+if($estado == "AC"){
+    $estado = "Acre";
+} elseif($estado == "AL"){
+    $estado = "Alagoas";
+} elseif($estado == "AP"){
+    $estado = "Amapá";
+} elseif($estado == "AM"){
+    $estado = "Amazonas";
+} elseif($estado == "BA"){
+    $estado = "Bahia";
+} elseif($estado == "CE"){
+    $estado = "Ceará";
+} elseif($estado == "DF"){
+    $estado = "Distrito Federal";
+} elseif($estado == "ES"){
+    $estado = "Espírito Santo";
+} elseif($estado == "GO"){
+    $estado = "Goiás";
+} elseif($estado == "MA"){
+    $estado = "Maranhão";
+} elseif($estado == "MT"){
+    $estado = "Mato Grosso";
+} elseif($estado == "MS"){
+    $estado = "Mato Grosso do Sul";
+} elseif($estado == "MG"){
+    $estado = "Minas Gerais";
+} elseif($estado == "PA"){
+    $estado = "Pará";
+} elseif($estado == "PB"){
+    $estado = "Paraíba";
+} elseif($estado == "PR"){
+    $estado = "Paraná";
+} elseif($estado == "PE"){
+    $estado = "Pernambuco";
+} elseif($estado == "PI"){
+    $estado = "Piauí";
+} elseif($estado == "RJ"){
+    $estado = "Rio de Janeiro";
+} elseif($estado == "RN"){
+    $estado = "Rio Grande do Norte";
+} elseif($estado == "RS"){
+    $estado = "Rio Grande do Sul";
+} elseif($estado == "RO"){
+    $estado = "Rondônia";
+} elseif($estado == "RR"){
+    $estado = "Roraima";
+} elseif($estado == "SC"){
+    $estado = "Santa Catarina";
+} elseif($estado == "SP"){
+    $estado = "São Paulo";
+} elseif($estado == "SE"){
+    $estado = "Sergipe";
+} elseif($estado == "TO"){
+    $estado = "Tocantins";
+}
+
 //Se houver um novo avatar enviado, delete o antigo e atualize um novo
-if($_FILES['file']['name'] != ""){
+if($_FILES['arquivo']['name'] != ""){
     //pegando a extensão da imagem
-    $separa = explode(".", $_FILES['file']['name']);
+    $separa = explode(".", $_FILES['arquivo']['name']);
     $separa = array_reverse($separa);
     $tipoimg = $separa[0];
     $ext = strtolower("." . $tipoimg);
@@ -34,16 +102,16 @@ if($_FILES['file']['name'] != ""){
         $limite = 10240000; 
         
         //Definindo o tamanho em uma variavel
-        $tamanhoImg = $_FILES['file']['size']; 
+        $tamanhoImg = $_FILES['arquivo']['size']; 
         
         if($tamanhoImg <= $limite){
             //pegando o nome da imgagem no Banco de dados
-            $img = $mysql->prepare("SELECT img FROM ong WHERE id = $id");
+            $img = $mysql->prepare("SELECT ong_img FROM ong WHERE id = $id");
             $img->execute();
 
             //verificando se existe uma imagem no bd
             if($linha = $img->fetch(PDO::FETCH_ASSOC)){
-                $name = $linha['img']; //Se existir vai entrar na variavel $name
+                $name = $linha['ong_img']; //Se existir vai entrar na variavel $name
             }
 
             if(file_exists("../../imgs/$name")) { //verificando se ela existe no diretorio
@@ -61,14 +129,28 @@ if($_FILES['file']['name'] != ""){
             $newNameImg = md5($nameOng) . time() . $ext;
 
             //Upando a imagem no repositorio
-            move_uploaded_file($_FILES['file']['tmp_name'], $uploaddir . $newNameImg);
+            move_uploaded_file($_FILES['arquivo']['tmp_name'], $uploaddir . $newNameImg);
+
+            //tirando traço do cep
+            $CEPAlterado = str_replace('-', '', $CEP);
 
             //query para atualizar a imagem do bd
-            $sql = "UPDATE ong SET img = ? WHERE id = ?";
+            $sql = "UPDATE ong 
+                      SET ong_description = ?,
+                      ong_purpose = ?,
+                      ong_phone = ?,
+                      ong_business_hours = ?,
+                      location_cep = ?,
+                      location_address = ?,
+                      location_number = ?,
+                      location_district = ?,
+                      location_state = ?,
+                      ong_img = ? 
+                        WHERE id = ?";
             $stmt = $mysql->prepare($sql);
             
             //executar o update
-            $stmt->execute([$newNameImg, $id]);
+            $stmt->execute([$descricao, $proposito, $telefone, $horarioFunc, $CEPAlterado, $rua, $numero, $bairro, $estado, $newNameImg, $id]);
             
             if($stmt){
                 echo "<script language='javascript' type='text/javascript'>alert('Atualização feita com sucesso!'); window.location = ' ../perfil.php'; </script>";
@@ -81,6 +163,27 @@ if($_FILES['file']['name'] != ""){
     } else {
         echo "<script language='javascript' type='text/javascript'>alert('O arquivo não é uma imagem, por favor faça o upload de uma imagem .png, .jpg ou .svg . Extensão atual: $ext'); window.location = ' ../perfil.php';</script>";
     }
-} elseif($_FILES['file']['error'] == '4'){
-    echo "<script language='javascript' type='text/javascript'>alert('Nenhuma imagem foi upada, tente novamente!'); window.location = ' ../perfil.php';</script>";
+} elseif($_FILES['arquivo']['error'] == '4'){
+    $CEPAlterado = str_replace('-', '',$CEP);
+    $sql = "UPDATE ong 
+              SET ong_description = ?,
+              ong_purpose = ?,
+              ong_phone = ?,
+              ong_business_hours = ?,
+              location_cep = ?,
+              location_address = ?,
+              location_number = ?,
+              location_district = ?,
+              location_state = ? 
+                WHERE id = ?";
+    $stmt = $mysql->prepare($sql);
+
+    $stmt->execute([$descricao, $proposito, $telefone, $horarioFunc, $CEPAlterado, $rua, $numero, $bairro, $estado, $id]);
+    
+    if($stmt){
+        echo "<script language='javascript' type='text/javascript'>alert('Atualização feita com sucesso!'); window.location = ' ../perfil.php';</script>";
+    }else{
+        echo "<script language='javascript' type='text/javascript'>alert('Não foi possivel fazer a atualização da adoção!'); window.location = ' ../perfil.php';</script>";
+    }
 }
+
