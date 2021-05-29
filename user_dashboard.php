890579daadefabb9ac1a11a12ec6d3cd24181dce
IgorSPessoa <igor_pessoa2011@outlook.com>
2021-05-29 02:27:59 -0300

    user dashboard

--- user_dashboard.php
+++ user_dashboard.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="css/resetcss.css">
     <link rel="stylesheet" href="css/all.css">
     <link rel="stylesheet" href="css/user_dashboard.css">
+    <link rel="stylesheet" href="css/maps.css">
 
     <title>Home User</title>
 </head>
@@ -40,26 +41,251 @@
         echo "<script language='javascript' type='text/javascript'>window.location = ' login.php';</script>";
     }
     ?>
-    <main>
-        <div class="d-flex justify-content-center">
-            <img src="./imgs/user.gif" alt="">
+    <main class="bg-dark">
+        <div class="d-flex justify-content-center ">
+            <img src="./imgs/user.gif" class="perfilimg rounded-circle" alt="foto">
         </div>
-        <div class="User bg-dark">
-            <h2><?php echo $_SESSION['name']; ?></h2>
-            <p>Reports enviados: 90</p>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <br>
-            <div class="d-flex flex-row-reverse">
-                <a href="logout.php" class="button btn btn-danger"> Sair</a>
+        <div class="d-flex justify-content-center">
+            <div class="w-75 bg-white shadow lg-3 border border-3 border-primary px-5 py-2">
+                <div class="d-flex flex-row-reverse">
+                    <a href="logout.php" class="button btn btn-danger"> Sair</a>
+                </div>
+                <div class="User ">
+                    <h2><?php echo $_SESSION['name']; ?></h2>
+                </div>
+                <?php
+                //conexão com banco de dados
+                include "connect.php";
+
+                $id_user = $_SESSION['id'];
+                $name = $_SESSION['name'];
+                $email = $_SESSION['email'];
+
+                $sql = $mysql->prepare("SELECT * FROM tcc.user where id= $id_user");
+                $sql->execute();
+                while ($linha = $sql->fetch(PDO::FETCH_ASSOC)) {
+                    $pwd_user = $linha['pwd'];
+                    $img_user = $linha['img'];
+                    $phone_user = $linha['phone'];
+                    $cep_user = $linha['cep'];
+                    $dataconta_user = $linha['created_at'];
+
+                    echo "<h3>Informações</h3>
+                    <p>Email:$email</p>
+                    <p>Telefone:$phone_user</p>
+                    <p>Cep:$cep_user</p>
+                    <a hef='#' class='btn btn-danger'>Mudar dados</a>";
+                }
+                ?>
             </div>
+        </div>
+
+
+        <div class="d-flex justify-content-center">
+            <div class="w-75 bg-white shadow lg-3 border border-3 border-primary px-5 py-2">
+                <h4 class="text-center">Todos seus reports</h4>
+                <table id="table_idsnd" class="display">
+                    <thead>
+                        <tr>
+                            <th>Animal</th>
+                            <th>Descrição</th>
+                            <th>Localização</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $chave = "AIzaSyChFNJMuEdWzbDHzz1GskqtstVDLe9dcIo";
+                        $idmap = 1;
+                        //conexão com banco de dados
+
+                        //definir o número total de resultados que você deseja por página
+                        $results_per_page = 6;
+
+                        //encontre o número total de resultados armazenados no banco de dados  
+                        $query = $mysql->prepare("SELECT * FROM tcc.animal_report where author_id= $id_user");
+                        $query->execute();
+                        $number_of_result = $query->rowCount();
+
+                        //determinar o número total de páginas disponíveis
+                        $number_of_page = ceil($number_of_result / $results_per_page);
+
+                        //determinar em qual número de página o visitante está atualmente
+                        if (!isset($_GET['page'])) {
+                            $page = 1;
+                        } else {
+                            $page = $_GET['page'];
+                        }
+
+                        //determinar o número inicial de sql LIMIT para os resultados na página de exibição  
+                        $page_first_result = ($page - 1) * $results_per_page;
+
+                        //preparando querry com banco de dados, para pegar os reports feito pelo usuario
+                        $result = $mysql->prepare("SELECT * FROM tcc.animal_report where author_id= $id_user LIMIT " . $page_first_result . ',' . $results_per_page);
+                        $result->execute();
+                        while ($linha = $result->fetch(PDO::FETCH_ASSOC)) {
+                            $id = $linha['id'];
+                            $author_id = $linha['author_id'];
+                            $idOngReport = $linha['ong_id'];
+                            $animal = ucfirst($linha['animal_type']);
+                            $description = $linha['animal_description'];
+                            $situacaoReport = $linha['report_situation'];
+                            $imgAnimal = $linha['animal_photo'];
+                            $cep = $linha['location_cep'];
+                            $location = "$linha[location_address] $linha[location_number], $linha[location_district], $linha[location_state]";
+                            $imgLocation = $linha['location_photo'];
+                            $pointOfReference = $linha['location_observation'];
+                            $data_aceite = $linha['report_date_accepted'];
+                            $situaReport = $linha['report_situation'];
+                            $comments = $linha['report_comments'];
+                            $imagemReport = $linha['report_img'];
+
+                            //mudando a variavel para português
+                            if ($animal == "dog") {
+                                $animal = "Cachorro";
+                            } else if ($animal == "cat") {
+                                $animal = "Gato";
+                            } else if ($animal == "others") {
+                                $animal  = "Outro";
+                            }
+
+                            echo '<tr>';
+                            echo '<td>' .  $animal . '</td>';
+                            echo '<td>' .  $description . '</td>';
+                            echo '<td>' .  $location . '</td>';
+                            echo "<td><a class='btn btn-success' data-toggle='modal' data-target='#Modal$id' onclick='loadmap(\"$location\",$idmap);'>Visualizar</a></td>";
+                            echo '</tr>';
+                            //modal
+                            echo "
+                            
+                            <div class='modal fade bd-example-modal-lg' id='Modal$id' tabindex='1' role='dialog' aria-labelledby='myLargeModalLabel' aria-hidden='true'>
+                                <div class='modal-dialog modal-lg'>
+                                    <div class='modal-content'>
+                                        <div class='modal-header'>
+                                            <h5 class='modal-title' id='TituloModalCentralizado'>Visualizar Report</h5>
+                                            <button type='button' class='close' data-dismiss='modal' aria-label='Fechar'>
+                                                <span aria-hidden='true'>&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class='modal-body'>
+                                            <form>
+                                                <!-- divs para colocar o autor, telefone e o animal em linhas até 576px -->
+                                                <div class='row'>
+                                                    <div class='col-sm'>
+                                                        <label for='author_name' class='m-0'>
+                                                            <h4>Autor</h4>
+                                                        </label>
+                                                        <input class='form-control w-100 mb-2' type='text' id='author_name' name='author_name' value='$name' readonly>
+                                                    </div>
+    
+                                                    <div class='col-sm'>
+                                                        <label for='animal_description' class='m-0'>
+                                                            <h4>Animal</h4>
+                                                        </label>
+                                                        <input class='form-control w-100 mb-2' type='text' id='animal_description' name='animal_description' value='$animal' readonly>
+                                                    </div>
+    
+                                                </div>
+    
+                                                <div class='form-group'>
+                                                    <label for='description' class='text-md'>
+                                                        <h4>Descrição</h4>
+                                                    </label>
+                                                    <input class='form-control w-100 mb-2' type='text' id='description' name='description' value='$description' readonly>
+                                                </div>
+    
+                                                <!-- divs para colocar a localização e o ponto de referência em linha até 576px -->
+                                                <div class='row'>
+                                                    <div class='col-sm'>
+                                                        <label for='localization_lastview' class='m-0'>
+                                                            <h4>Localização</h4>
+                                                        </label>
+                                                        <input class='form-control w-100 mb-2' type='text' id='localization_lastview' name='localization_lastview' value='$location' readonly>
+                                                    </div>
+    
+                                                    <div class='col-sm'>
+                                                        <label for='localization_observation' class='m-0'>
+                                                            <h4>Ponto de referência</h4>
+                                                        </label>
+                                                        <input class='form-control w-100 mb-2' type='text' id='localization_observation' name='localization_observation' value='$pointOfReference' readonly>
+                                                    </div>
+    
+                                                </div>
+    
+                                                <!-- divs para colocar as imagens em linhas até 576px -->
+                                                <div class='row'>
+    
+                                                    <div class='col-sm'>
+                                                        <h4 class='m-0'>Foto animal</h4>
+                                                        <img class='w-100 shadow border border-dark rounded ' src='./imgs/$imgAnimal'>
+                                                    </div>
+    
+                                                    <div class='col-sm'>
+                                                        <h4 class='m-0'>Foto do local</h4>
+                                                        <img class='w-100 shadow border border-dark rounded' src='./imgs/$imgLocation ?>'>
+                                                    </div>
+    
+    
+                                                    <div class='col-sm'>
+                                                        <h4 class='m-0'>Maps</h4>
+                                                        <div class='map2 w-100 shadow border border-dark rounded' id='map$idmap'></div>
+                                                    </div>
+    
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class='modal-footer'>
+                                            <button type='button' class='btn btn-secondary' data-dismiss='modal'>Fechar</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>";
+                            $idmap = $idmap + 1;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <div class="d-flex justify-content-center p-2">
+                    <?php
+                    //set de variaveis para paginação.
+                    $pagina_anterior = $page - 1;
+                    $pagina_proxima = $page + 1;
+                    $page_atual = $page;
+
+                    //Testa se pode ter o botão de pagina anterior ou não.
+                    if ($pagina_anterior != 0) {
+                        echo '<a href = "user_dashboard.php?page=' . $pagina_anterior . '" class="btn btn-success"> << </a>';
+                    } else {
+                        echo '<a href="#" class="btn btn-success disabled" role="button" aria-disabled="true"> << </a>';
+                    }
+                    //imprime os button de paginação até 5 (para limitar bloco de paginação).
+                    for ($page = 1; $page <= 5 && $page <= $number_of_page; $page++) {
+                        echo '<a href = "user_dashboard.php?page=' . $page . '" class="btn btn-success">' . $page . ' </a>';
+                    }
+                    /*
+                    Testa se o numero de paginas vai ser maior que 5 (para limitar bloco de paginação),
+                    se for ele imprime o button para a proxima pagina. 
+                    */
+                    if ($number_of_page > 5) {
+                        if ($page_atual < $number_of_page) {
+                            echo '<a href="#" class="btn btn-success disabled" role="button" aria-disabled="true">...</a>';
+                            echo '<a href = "user_dashboard.php?page=' . $pagina_proxima . '" class="btn btn-success">' . $pagina_proxima . ' </a>';
+                        } else {
+                            echo '<a href="#" class="btn btn-success disabled" role="button" aria-disabled="true">...</a>';
+                            echo '<a href = "user_dashboard.php?page=' . $page_atual . '" class="btn btn-success">' . $page_atual . ' </a>';
+                        }
+                    }
+                    //Testa se pode ter o botão de proxima pagina ou não.
+                    if ($pagina_proxima <= $number_of_page) {
+                        echo '<a href = "user_dashboard.php?page=' . $pagina_proxima . '" class="btn btn-success"> >> </a>';
+                    } else {
+                        echo '<a href="#" class="btn btn-success disabled" role="button" aria-disabled="true"> >> </a>';
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        </br>
         </div>
     </main>
     <?php
@@ -69,21 +295,27 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyChFNJMuEdWzbDHzz1GskqtstVDLe9dcIo"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        var chave = "<?= $chave ?>";
+    </script>
+    <script src="js/user_dashboard.js"></script>"
     <script src="dashboard/plugins/bootstrap/js/bootstrap.min.js"></script>
     <script src="js/global.js"></script>
-    <?php 
-        //verificando se existe uma mensagem para o usúario
-        if(isset($_GET['msg'])) {
-            
-            $msg = $_GET['msg']; // colocando a mensagem em uma variavel
-            $_COOKIE['msg'] = $msg; // Colocando a variavel em um cookie, para conseguir pegar no outro script
 
-            $name = $_SESSION['name'];// Colocando o nome do usúario em uma variavel 
-            $_COOKIE['nome'] = $name; // Pegando a variavel e enviando para outro script via SESSION 
- 
-            include 'includes/modal.php'; //incluindo o modal para a página
-        }
+    <?php
+    //verificando se existe uma mensagem para o usúario
+    if (isset($_GET['msg'])) {
+
+        $msg = $_GET['msg']; // colocando a mensagem em uma variavel
+        $_COOKIE['msg'] = $msg; // Colocando a variavel em um cookie, para conseguir pegar no outro script
+
+        $name = $_SESSION['name']; // Colocando o nome do usúario em uma variavel 
+        $_COOKIE['nome'] = $name; // Pegando a variavel e enviando para outro script via SESSION 
+
+        include 'includes/modal.php'; //incluindo o modal para a página
+    }
     ?>
 </body>
 
