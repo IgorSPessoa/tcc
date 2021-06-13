112e8fdac476b162331cffb6b8e067d3480c2e23
Guilherme Parnaíba <guilherme.lima285@etec.sp.gov.br>
2021-06-13 12:40:31 -0300

    API updates

---
+++ api/functions/utils/ping.php
@@ -0,0 +1,11 @@
+<?php
+// Headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+echo json_encode(array("message" => "pong"));   
+
+?>
